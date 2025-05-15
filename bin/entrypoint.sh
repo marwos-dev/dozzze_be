97f908a702f4fbe9d268b6451784962e576699d7
Marcos Olmedo <marcos@plum.com.ar>
2025-05-15 20:20:52 -0300

    fix

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -14,7 +14,7 @@ python manage.py shell << EOF
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "admin123")
+    User.objects.create_superuser("admin", "admin@example.com", "1234")
     print("Superusuario creado: admin / admin123")
 else:
     print("El superusuario ya existe.")
