314af53d1007b03b88dc92a878a9645b74df13b4
Marcos Olmedo <marcos@plum.com.ar>
2025-05-16 20:00:08 -0300

    fix production

--- core/settings.py
+++ core/settings.py
@@ -127,10 +127,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")  # ✔️ Aquí Django juntará los archivos
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static/']
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Eliminalo en producción 👇
+# STATICFILES_DIRS = [...]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
