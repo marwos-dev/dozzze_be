10f9948dd07e4f8ee8fad2607f9aa06b8187e55d
Marcos Olmedo <62622400+marwos-dev@users.noreply.github.com>
2025-07-24 14:35:16 -0300

    Fix recursive Django setup in celery

--- core/celery.py
+++ core/celery.py
@@ -1,12 +1,9 @@
 # myproject/celery.py
 import os
 
-import django
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-
-django.setup()
 
 app = Celery("core")
 
