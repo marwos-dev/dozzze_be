19b4c842c1771fee86b853bc31b6aaf9bc6ce7be
Marcos Olmedo <62622400+marwos-dev@users.noreply.github.com>
2025-07-24 14:41:10 -0300

    Fix admin classes to use GISModelAdmin

--- properties/admin.py
+++ properties/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin, messages
-from django.contrib.gis.admin import OSMGeoAdmin
+from django.contrib.gis.admin import GISModelAdmin
 from django.core.paginator import Paginator
 from django.http import HttpResponseRedirect
 from django.template.response import TemplateResponse
@@ -30,7 +30,7 @@ from .sync_service import SyncService
 
 
 @admin.register(Property)
-class PropertyAdmin(OSMGeoAdmin):
+class PropertyAdmin(GISModelAdmin):
     class Media:
         js = ("js/addPoligon.js",)
         css = {"all": ("css/map_solution.css",)}
